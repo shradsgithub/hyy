0180bbca8d86bfc122faed3b773fbe4e5b6cdf53
shradsgithub <shradsjobs@gmail.com>
2023-04-21 20:21:53 +0530

    2nd commit

---
+++ src/test/java/gitBashPractise/MrGitHub.java
@@ -0,0 +1,5 @@
+package gitBashPractise;
+
+public class MrGitHub {
+
+}
---
+++ src/test/java/gitBashPractise/MrGitHub2.java
@@ -0,0 +1,14 @@
+package gitBashPractise;
+
+import org.testng.annotations.Test;
+
+public class MrGitHub2 {
+	
+	@Test
+	public void god()
+	{
+		System.out.println("Enjoy");
+	}
+
+
+}
